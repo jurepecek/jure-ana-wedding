ab988e7a10c9c30bfa159b4330f08678108b0abc
jurepecek <59982011+jurepecek@users.noreply.github.com>
2026-02-05 18:49:12 +0100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    base: '', // Allows the app to be deployed to any subdirectory
-})
-
